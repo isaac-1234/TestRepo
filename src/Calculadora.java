5877fe5102300946975680e8e8bc3143fc5fea1b
isaac-1234 <isaac_7j@hotmail.com>
2023-06-04 01:58:48 -0600

    Creación del programa calculadora Sesión de Asesoría

---
+++ src/Calculadora.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+    public class Calculadora {
+        static double num1, num2, resultado;
+        static int operacion;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Bienvenido a la calculadora");
+        System.out.println("Ingresa tu primer numero");
+        num1 = Double.parseDouble(br.readLine());
+        System.out.println("Ingresa tu segundo numero");
+        num2 = Double.parseDouble(br.readLine());
+        System.out.println("Que operación quieres realizar");
+        System.out.println("1. +");
+        System.out.println("2. -");
+        System.out.println("3. *");
+        System.out.println("4. /");
+        operacion = Integer.parseInt(br.readLine());
+
+        switch (operacion) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                resultado = num1 / num2;
+                break;
+            default:
+                System.out.println("Seleccionaste una opcion incorrecta, Adios");
+        }
+
+        System.out.println("Tu resultado es: " + resultado);
+    }
+
+}
